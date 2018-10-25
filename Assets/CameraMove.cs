bfee32bff16e2eb0fd5c7e5dd38ced054a36cbef
Emma Frantz <ecfrantz@mymail.mines.edu>
2018-10-25 17:14:23 -0600

    first commit

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    public float moveSpeed2;
+
+    // Use this for initialization
+    private void Start()
+    {
+        moveSpeed2 = 10f;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * moveSpeed2 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * moveSpeed2 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * moveSpeed2 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * moveSpeed2 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.Q))
+            transform.Translate(Vector3.down * moveSpeed2 * Time.deltaTime);
+        if (Input.GetKey(KeyCode.E))
+            transform.Translate(Vector3.up * moveSpeed2 * Time.deltaTime);
+    }
+}
---
+++ Assets/CubeMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+
+    public float moveSpeed;
+
+    // Use this for initialization
+    private void Start () {
+        moveSpeed = 10f;
+	}
+
+    // Update is called once per frame
+    private void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.RightShift))
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightShift))
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+    }
+}
