d84e13353efd330715d2fdbc93a6fbc6922d9089
Emma Frantz <ecfrantz@mymail.mines.edu>
2018-10-29 18:53:01 -0600

    Added code for login

--- Assets/DataRecordExp.cs
+++ Assets/DataRecordExp.cs
@@ -6,17 +6,20 @@ using System.IO;
 using System.Collections;
 using System.Text;
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.UI;
 
 public class DataRecordExp : MonoBehaviour {
 
     public Text output;
+    static string path = "Assets/DataOut.txt";
+    //StreamWriter writer = new StreamWriter(path, true);
     //const string OutFilePath = @"EEGLogger.csv";
     //private static FileStream OutFileStream;
-    //const string Username = "your_username";
-    //const string Password = "your_password";
-    //const string LicenseId = "your_license";
-    //const int DebitNumber = 2; // default number of debit
+    const string Username = "your_username";
+    const string Password = "your_password";
+    const string LicenseId = "your_license";
+    const int DebitNumber = 2; // default number of debit
 
     // Use this for initialization
     //private void Start () {
@@ -26,6 +29,9 @@ public class DataRecordExp : MonoBehaviour {
 	// Update is called once per frame
 	public void Record () {
         output.text = "Please make sure the headset has good signal.";
+
+        //writer.WriteLine("Testing testing 123");
+        //writer.Close();
 
         // Delete Output file if existed
         //if (File.Exists(OutFilePath))
@@ -40,21 +46,21 @@ public class DataRecordExp : MonoBehaviour {
         //p.OnEEGDataReceived += OnEEGDataReceived;
         //p.SessionCtr.OnSubcribeEEGOK += OnEEGDataReceived;
 
-        //Thread.Sleep(10000); //wait for querrying user login, query headset
-        //if (String.IsNullOrEmpty(p.GetUserLogin()))
-        //{
-        //    p.Login(Username, Password);
-        //    Thread.Sleep(5000); //wait for logining
-        //}
-        //// Show username login
-        //output.text = "Username :" + p.GetUserLogin();
+        Thread.Sleep(10000); //wait for querrying user login, query headset
+        if (String.IsNullOrEmpty(p.GetUserLogin()))
+        {
+            p.Login(Username, Password);
+            Thread.Sleep(5000); //wait for logining
+        }
+        // Show username login
+        output.text = "Username :" + p.GetUserLogin();
 
-        //if (p.AccessCtr.IsLogin)
-        //{
-        //    // Send Authorize
-        //    p.Authorize(LicenseId, DebitNumber);
-        //    Thread.Sleep(5000); //wait for authorizing
-        //}
+        if (p.AccessCtr.IsLogin)
+        {
+            // Send Authorize
+            p.Authorize(LicenseId, DebitNumber);
+            Thread.Sleep(5000); //wait for authorizing
+        }
         //if (!p.IsHeadsetConnected())
         //{
         //    p.QueryHeadset();
